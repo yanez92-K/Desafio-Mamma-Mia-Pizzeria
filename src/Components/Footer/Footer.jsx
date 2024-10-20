7d2d13e0c79b2a0167a1c63d0e360c27ac8707d6
Yanez92-k <Yanez.Peredo.P.K@outlook.com>
2024-10-20 10:59:27 -0300

    Update Footer and Title

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer>
       <p className="text-light text-center  bg-dark p-2 p-md-4">
-        © 2021 - Pizzería Mamma Mia! - Todos los derechos reservados
+        © 2024 - Pizzería Mamma Mia! - Todos los derechos reservados
       </p>
     </footer>
   );
